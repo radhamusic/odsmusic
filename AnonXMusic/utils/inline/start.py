e447dfdcb0fb1212a8c303a5d1513d8995700f58
LucyXDbot <lucymorningstar.9783@gmail.com>
2024-09-04 13:36:10 +0530

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -31,6 +31,6 @@ def private_panel(_):
             InlineKeyboardButton(text=_["S_B_5"], url=config.SUPPORT_CHANNEL),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT)],
         ]
     return buttons
