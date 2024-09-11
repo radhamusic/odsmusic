ce87fb7763b1130bc1fe64c007493a097546e1aa
LucyXDbot <lucymorningstar.9783@gmail.com>
2024-09-11 23:01:01 +0530

    Update play.py

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -66,9 +66,9 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-            InlineKeyboardButton(text="🇴ｌｄ ×፝֟͜× 🇸ｋｏｏｌ", url=f"https://t.me/oldskoolgc")],
+           [InlineKeyboardButton(text="🇴ｌｄ ×፝֟͜× 🇸ｋｏｏｌ", url=f"https://t.me/oldskoolgc")],
        [
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],    ]
+        InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],    ]
     
     return buttons
 
