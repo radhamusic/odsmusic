a0e6a827f73a9b74262774822902d2923a517f74
radhamusic <radhamusic5911@gmail.com>
2024-11-25 13:44:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/dhruvods/NexMusic",
+    "https://github.com/radhamusic/odsmusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
